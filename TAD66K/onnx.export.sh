a2c5eca7bdfc7af2a29d836ec75e8ef7d0a59dba
xxai.art <xxai.art@gmail.com>
2023-08-02 02:46:37 +0800

    ♨

---
+++ TAD66K/onnx.export.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+DIR=$(realpath $0) && DIR=${DIR%/*}
+cd $DIR
+set -ex
+
+rm -rf ./onnx
+./onnx_export.py
+cd onnx/eta
+onnx=tad66k.onnx
+mod=onnxoptimizer
+python -m $mod $onnx $mod.$onnx
+mv $mod.$onnx $onnx
